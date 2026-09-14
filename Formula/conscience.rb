604a200c70a5f97ae6018774f732187b1dad82e0
Matt Konda <550691+mkonda@users.noreply.github.com>
2026-09-14 20:07:36 +0900

    Update conscience to 0.4.0

--- Formula/conscience.rb
+++ Formula/conscience.rb
@@ -1,21 +1,21 @@
 class Conscience < Formula
   desc "Evaluate the impact of AI-assisted work"
   homepage "https://github.com/subversivesoftwareorg/conscience"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/subversivesoftwareorg/conscience/releases/download/v#{version}/conscience-macos-arm64"
-    sha256 "95177945662c91a71f2f92b272c94533c80539db0260721c30106928711d589b"
+    url "https://github.com/subversivesoftwareorg/conscience/releases/download/v0.4.0/conscience-macos-arm64"
+    sha256 "cf8e6b4aa8f94e6c34681fcd3bb5d6b5616e42bd2b85e8c8b17f1a558c749163"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/subversivesoftwareorg/conscience/releases/download/v#{version}/conscience-linux-arm64"
-    sha256 "905f5d4c26d20d7b9aea0ac81671d584a4cdd90ef69c1b8b46639cfb9c22c55f"
+    url "https://github.com/subversivesoftwareorg/conscience/releases/download/v0.4.0/conscience-linux-arm64"
+    sha256 "954c09f1bae47dc6b3de457170a3556c1cbd3e3d3c72602add3171465d87ca9f"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/subversivesoftwareorg/conscience/releases/download/v#{version}/conscience-linux-amd64"
-    sha256 "658d44bef1c79c35a49c34f875e59cf95ca98732498d37c6a631d9bd6ead9f1d"
+    url "https://github.com/subversivesoftwareorg/conscience/releases/download/v0.4.0/conscience-linux-amd64"
+    sha256 "896e827d6220bfb7dbfd690f81e8902e04e711c3eef12486c60d63b49025c034"
   else
-    url "https://github.com/subversivesoftwareorg/conscience/archive/refs/tags/v#{version}.tar.gz"
-    sha256 "3f426b290afbe25de79505040b96e4fcda9ad6f593db79f7af7d026ea1b126f0"
+    url "https://github.com/subversivesoftwareorg/conscience/archive/refs/tags/v0.4.0.tar.gz"
+    sha256 "e9af0557fdef1c009441c6f2d24722ababbad11e680afe1e4c56bb7560447eb5"
     depends_on "rust" => :build
   end
 
